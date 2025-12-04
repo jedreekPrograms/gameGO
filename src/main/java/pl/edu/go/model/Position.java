2d84936fa5744a836acfe00cae3aa56f54e7a328
Jedrek <jedreek323@gmail.com>
2025-12-04 21:21:30 +0100

    dodalem tu hash code zeby pozycje te same byly traktowane jako to samo i nadpisalem toString

--- src/main/java/pl/edu/go/model/Position.java
+++ src/main/java/pl/edu/go/model/Position.java
@@ -1,4 +1,35 @@
 package pl.edu.go.model;
 
-public class Position {
+import java.util.Objects;
+
+public final class Position {
+    public final int x;
+    public final int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+
+        if (!(o instanceof Position)) return false;
+
+        Position pos = (Position) o;
+
+        return x == pos.x && y == pos.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
 }
