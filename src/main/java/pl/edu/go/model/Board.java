e801b64013af573cc7e8d5801652832e52f9b4e4
Jedrek <jedreek323@gmail.com>
2025-12-05 01:59:44 +0100

    x

--- src/main/java/pl/edu/go/model/Board.java
+++ src/main/java/pl/edu/go/model/Board.java
@@ -1,7 +1,18 @@
 package pl.edu.go.model;
 
 public class Board {
+    public Board(int size) {
+    }
+
     public int countLiberties(int sx, int sy) {
 
+        return sx;
+    }
+
+    public Integer computeHash() {
+        return 0;
+    }
+
+    public int placeStone(Color color, int x, int y) {
     }
 }
--- src/main/java/pl/edu/go/model/Move.java
+++ src/main/java/pl/edu/go/model/Move.java
@@ -1,4 +1,52 @@
 package pl.edu.go.model;
 
 public class Move {
+    //place postawienie np (3,5)
+    //pass to ze rezygunuje z ruchu
+    //resign poddaje sie
+    public enum Type {PLACE, PASS, RESIGN}
+
+    private final Type type;
+    private final Position pos;
+    private final Color color;
+
+    public Move(Type type, Position pos, Color color) {
+        this.type = type;
+        this.pos = pos;
+        this.color = color;
+    }
+
+    public static Move place(Position pos, Color color) {
+        return new Move(Type.PLACE, pos, color);
+    }
+
+    public static Move pass(Color color) {
+        return new Move(Type.PASS, null, color);
+    }
+
+    public static Move resign (Color color) {
+        return new Move(Type.RESIGN, null, color);
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Position getPos() {
+        return pos;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        switch (type) {
+            case PLACE: return color + " ->" + pos;
+            case PASS: return color + " passes";
+            case RESIGN: return color + " resigns";
+            default: return "";
+        }
+    }
 }
