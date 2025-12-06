32cad75014f32c4da40cd21e6d1fbbeb3cd2e364
Jedrek <jedreek323@gmail.com>
2025-12-06 18:28:28 +0100

    sendMove wysyła ruch do serwera

--- src/main/java/pl/edu/go/client/NetworkHandler.java
+++ src/main/java/pl/edu/go/client/NetworkHandler.java
@@ -1,4 +1,32 @@
 package pl.edu.go.client;
 
+import pl.edu.go.model.Move;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class NetworkHandler {
+    private final Socket socket;
+    private final ObjectOutputStream out;
+    private final ObjectInputStream in;
+
+    public NetworkHandler(String host, int port) throws IOException {
+        this.socket = new Socket(host, port);
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+        this.in = new ObjectInputStream(socket.getInputStream());
+    }
+
+    /**
+     * Wysyła ruch do serwera.
+     *
+     * @param move obiekt Move do wysłania
+     * @throws IOException jak będzie problem z wysłaniem
+     */
+    public void sendMove(Move move) throws IOException {
+        out.writeObject(move);
+        out.flush();
+    }
 }
