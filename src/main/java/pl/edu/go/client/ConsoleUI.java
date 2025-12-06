5ee7b72e478c0c227dcc6935fa37df1c2aaf1a7f
Jedrek <jedreek323@gmail.com>
2025-12-06 19:13:46 +0100

    displayBoard do wyswietlania planszy w formacie:                                      0 1 2 3 4     0 B . W . .     1 . B W . . itd

--- src/main/java/pl/edu/go/client/ConsoleUI.java
+++ src/main/java/pl/edu/go/client/ConsoleUI.java
@@ -1,4 +1,41 @@
 package pl.edu.go.client;
 
+import pl.edu.go.model.Board;
+import pl.edu.go.model.Color;
+
+import java.util.Scanner;
+
 public class ConsoleUI {
+
+    private final Scanner scanner;
+
+
+    public ConsoleUI(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void displayBoard(Board board) {
+        int size = board.getSize();
+        System.out.print("  ");
+        for (int x = 0; x < size; x++) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+        for (int y = 0; y < size; y++) {
+            System.out.print(y + " ");
+            for (int x = 0; x < size; x++) {
+                Color c = board.get(x, y);
+                char symbol;
+
+                switch(c) {
+                    case BLACK: symbol = 'B'; break;
+                    case WHITE: symbol = 'W'; break;
+                    default: symbol = '.'; break;
+                }
+                System.out.print(symbol + " ");
+            }
+            System.out.println();
+        }
+    }
 }
